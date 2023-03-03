3d56b549d2e354c0652774cafc2bf7f11af9a05f
Nestor162 <nestorcicardini@gmail.com>
2023-03-03 18:06:27 +0100

    Cambiamento colore bottone della nav bar

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -3,7 +3,11 @@ var scrollTrigger = 390;
 window.onscroll = function () {
   if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
     document.querySelector("header").classList.add("white");
+    document.querySelector(".header__nav-item--button").classList.add("green");
   } else {
     document.querySelector("header").classList.remove("white");
+    document
+      .querySelector(".header__nav-item--button")
+      .classList.remove("green");
   }
 };
